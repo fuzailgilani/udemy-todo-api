3a084f55d5dec098f16e901dd529d743243bd04e
Fuzail <fuzailgilani@gmail.com>
2018-06-15 13:58:51 -0500

    added update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b22d97286b9262e3c29e36a')
+  }, {
+    $set: {
+      name: 'Fuzail'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
